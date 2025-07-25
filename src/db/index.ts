0949de165474f3477582797e7d0921460e56afdd
NeilRow56 <neilrowland56@gmail.com>
2025-07-25 09:00:02 +0100

    Added drizzle and better-auth

---
+++ src/db/index.ts
@@ -0,0 +1,8 @@
+import { config } from "dotenv";
+import { drizzle } from "drizzle-orm/neon-http";
+import { schema } from "./schema";
+import { env } from "@/lib/env";
+
+config({ path: ".env" }); // or .env.local
+
+export const db = drizzle(env.DATABASE_URL!, { schema });
